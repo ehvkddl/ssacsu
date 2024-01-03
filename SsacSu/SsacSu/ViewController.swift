bf35ef4a4c22168ea2f3eb749a0a0a1714b93635
ehvkddl <ehvkddl@naver.com>
2024-01-03 22:42:29 +0900

    init: SsacSu 생성

---
+++ SsacSu/SsacSu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SsacSu
+//
+//  Created by do hee kim on 2024/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
