736eaf38ce89598ed30b8cd723980ea2ad84f0f6
ehvkddl <ehvkddl@naver.com>
2024-01-04 03:45:54 +0900

    chore: Color assets 추가 #1

---
+++ SsacSu/SsacSu/Colors.swift
@@ -0,0 +1,36 @@
+//
+//  Colors.swift
+//  SsacSu
+//
+//  Created by do hee kim on 2024/01/04.
+//
+
+import UIKit
+
+enum Colors {
+    
+    enum Brand {
+        static let black = UIColor(named: "Black")
+        static let error = UIColor(named: "Error")
+        static let gray = UIColor(named: "Gray")
+        static let inactive = UIColor(named: "Inactive")
+        static let white = UIColor(named: "White")
+        static let yellow = UIColor(named: "Yellow")
+    }
+    
+    enum Text {
+        static let primary = UIColor(named: "TXTPrimary")
+        static let secondary = UIColor(named: "TXTSecondary")
+    }
+    
+    enum Background {
+        static let primary = UIColor(named: "BGPrimary")
+        static let secondary = UIColor(named: "BGSecondary")
+    }
+    
+    enum View {
+        static let separator = UIColor(named: "Separator")
+        static let alpha = UIColor(named: "Alpha")
+    }
+    
+}
