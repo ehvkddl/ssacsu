35ec44885634df325120c192ff48d5978be8ca28
ehvkddl <ehvkddl@naver.com>
2024-01-05 05:00:05 +0900

    chore: 네트워크 통신 위한 설정 #1

---
+++ SsacSu/SsacSu/App/Configurations.swift
@@ -0,0 +1,26 @@
+//
+//  Configurations.swift
+//  SsacSu
+//
+//  Created by do hee kim on 2024/01/05.
+//
+
+import Foundation
+
+enum Configurations {
+    
+    static let SeSACKey: String = {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "SeSAC_KEY") as? String else {
+            fatalError("SeSACKey must not be empty in plist")
+        }
+        return key
+    }()
+    
+    static let baseURL: String = {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String else {
+            fatalError("baseURL must not be empty in plist")
+        }
+        return key
+    }()
+    
+}
