9e4289dc443005ded2c560e96181b3f9e83dfb27
ehvkddl <ehvkddl@naver.com>
2024-01-08 03:10:24 +0900

    chore: BaseView 추가 #1

---
+++ SsacSu/SsacSu/Presentation/Utils/Base/BaseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseTableViewCell.swift
+//  SsacSu
+//
+//  Created by do hee kim on 2024/01/08.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureView() { }
+    
+    func setConstraints() { }
+    
+}
---
+++ SsacSu/SsacSu/Presentation/Utils/Base/BaseViewController.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  SsacSu
+//
+//  Created by do hee kim on 2024/01/08.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+
+class BaseViewController: UIViewController {
+
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .Background.primary
+        
+        configureView()
+        setConstraints()
+        configureNavigationBar()
+    }
+    
+    func configureView() {}
+    
+    func setConstraints() {}
+    
+    func configureNavigationBar() {
+        navigationController?.navigationBar.tintColor = .Brand.black
+    }
+    
+}
