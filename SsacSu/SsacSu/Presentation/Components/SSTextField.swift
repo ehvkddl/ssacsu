f777c0f1a9d2d24d0fb267c9e520091263cfcd11
ehvkddl <ehvkddl@naver.com>
2024-01-10 03:05:24 +0900

    design: SSTextField 구현 #8
    - Inset이 있는 textField

---
+++ SsacSu/SsacSu/Presentation/Components/SSTextField.swift
@@ -0,0 +1,38 @@
+//
+//  SSTextField.swift
+//  SsacSu
+//
+//  Created by do hee kim on 2024/01/09.
+//
+
+import UIKit
+
+class SSTextField: UITextField {
+    
+    let _placeholder: String
+    let insets: UIEdgeInsets = UIEdgeInsets(top: 14, left: 12, bottom: 14, right: 12)
+    
+    init(placeholder _placeholder: String) {
+        self._placeholder = _placeholder
+        
+        super.init(frame: .zero)
+        
+        placeholder = _placeholder
+        font = SSFont.style(.body)
+        backgroundColor = .Background.secondary
+        layer.cornerRadius = 8
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: insets)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: insets)
+    }
+    
+}
