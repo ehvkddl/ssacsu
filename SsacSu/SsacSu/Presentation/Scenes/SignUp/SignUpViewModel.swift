c53fe03e25a97eaeb8cc32e4167c656ad257f159
ehvkddl <ehvkddl@naver.com>
2024-01-12 05:36:00 +0900

    feat: 이메일 정규식 변경 #9

--- SsacSu/SsacSu/Presentation/Scenes/SignUp/SignUpViewModel.swift
+++ SsacSu/SsacSu/Presentation/Scenes/SignUp/SignUpViewModel.swift
@@ -105,7 +105,7 @@ class SignUpViewModel: ViewModelType {
 extension SignUpViewModel {
     
     func emailValidation(email: String) -> Bool {
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.com"
         return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
     }
     
