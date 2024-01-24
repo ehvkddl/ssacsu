59fa105a362552c913913d11cc2a6b546367be70
ehvkddl <ehvkddl@naver.com>
2024-01-24 20:19:07 +0900

    feat: 토큰 갱신 기능 구현 #17

---
+++ SsacSu/SsacSu/Data/Network/TokenRefreshDTO.swift
@@ -0,0 +1,12 @@
+//
+//  TokenRefreshDTO.swift
+//  SsacSu
+//
+//  Created by do hee kim on 2024/01/24.
+//
+
+import Foundation
+
+struct TokenRefreshDTO: Decodable {
+    let accessToken: String
+}
