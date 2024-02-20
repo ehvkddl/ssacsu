f642f92e2cb141d5e17983139add32b19cc39657
ehvkddl <ehvkddl@naver.com>
2024-02-21 05:12:33 +0900

    design: ChattingView textView #22

--- SsacSu/SsacSu/Presentation/Scenes/Chatting/ChattingViewController.swift
+++ SsacSu/SsacSu/Presentation/Scenes/Chatting/ChattingViewController.swift
@@ -13,10 +13,43 @@ final class ChattingViewController: BaseViewController {
     
     let navigationBar = {
         let navBar = CommonNavigationBar(title: "title",
-                                      leftItemImage: .back,
-                                      rightItemImage: .list)
+                                         leftItemImage: .back,
+                                         rightItemImage: .list)
         return navBar
     }()
+    
+    let chatBox = {
+        let view = UIView()
+        view.backgroundColor = .Background.primary
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    let plusButton = {
+        let btn = UIButton()
+        btn.setImage(.plus, for: .normal)
+        return btn
+    }()
+    
+    let sendButton = {
+        let btn = UIButton()
+        btn.setImage(.ic, for: .normal)
+        return btn
+    }()
+    
+    let textView = {
+        let tv = UITextView()
+        tv.backgroundColor = .clear
+//        tv.backgroundColor = .View.alpha
+        tv.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tv.isScrollEnabled = false
+        
+        tv.showsVerticalScrollIndicator = false
+        tv.font = SSFont.style(.body)
+        return tv
+    }()
+    
+    let maxHeight: CGFloat = 46.6
     
     static func create(
         with viewModel: ChattingViewModel
@@ -28,6 +61,8 @@ final class ChattingViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .Background.secondary
         
         bind()
     }
@@ -44,10 +79,36 @@ final class ChattingViewController: BaseViewController {
             .asDriver(onErrorJustReturn: "채널명")
             .drive(navigationBar.title.rx.text)
             .disposed(by: disposeBag)
+        
+        textView.rx.didChange
+            .bind(with: self) { owner, _ in
+                let size = CGSize(width: owner.textView.frame.width, height: .infinity)
+                let estimatedSize = owner.textView.sizeThatFits(size)
+                
+                let isMaxHeight = estimatedSize.height >= owner.maxHeight
+                
+                guard isMaxHeight != owner.textView.isScrollEnabled else { return }
+                
+                owner.textView.isScrollEnabled = isMaxHeight
+                owner.textView.layoutIfNeeded()
+            }
+            .disposed(by: disposeBag)
+        
+        textView.rx.text.orEmpty
+            .bind(with: self) { owner, str in
+                let image: UIImage = str.count > 0 ? .icActive : .ic
+                
+                owner.sendButton.setImage(image, for: .normal)
+            }
+            .disposed(by: disposeBag)
     }
     
     override func configureView() {
-        [navigationBar].forEach { view.addSubview($0) }
+        [plusButton, textView, sendButton].forEach { chatBox.addSubview($0) }
+        
+        [navigationBar,
+         chatBox
+        ].forEach { view.addSubview($0) }
     }
     
     override func setConstraints() {
@@ -55,6 +116,34 @@ final class ChattingViewController: BaseViewController {
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.horizontalEdges.equalToSuperview()
             make.height.equalTo(44)
+        }
+        
+        chatBox.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view).inset(16)
+            make.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-12)
+            make.height.greaterThanOrEqualTo(38)
+        }
+        
+        plusButton.snp.makeConstraints { make in
+            make.top.greaterThanOrEqualTo(chatBox).inset(9)
+            make.leading.equalTo(chatBox).inset(12)
+            make.bottom.equalTo(chatBox).inset(9)
+            make.width.equalTo(22)
+        }
+        
+        sendButton.snp.makeConstraints { make in
+            make.top.greaterThanOrEqualTo(chatBox).inset(7)
+            make.trailing.equalTo(chatBox).inset(12)
+            make.bottom.equalTo(chatBox).inset(7)
+            make.width.equalTo(24)
+        }
+        
+        textView.snp.makeConstraints { make in
+            make.verticalEdges.equalTo(chatBox).inset(10)
+            make.leading.equalTo(plusButton.snp.trailing).offset(8)
+            make.trailing.equalTo(sendButton.snp.leading).offset(-8)
+            make.centerY.equalTo(chatBox)
+            make.height.lessThanOrEqualTo(maxHeight)
         }
     }
     
